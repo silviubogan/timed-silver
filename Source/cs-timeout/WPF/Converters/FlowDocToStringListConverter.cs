37a8f728acfdcf575595e6eff578a803701bc57f
Silviu Bogan <silviubogan@gmail.com>
2019-10-31 13:12:17 +0200

    Single drag indicator works by removing the formatting of the clock Tag

---
+++ Source/cs-timeout/WPF/Converters/FlowDocToStringListConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+namespace cs_timed_silver
+{
+    public class FlowDocToStringListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is FlowDocument))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            var l = new List<string>();
+            l.Add((value as FlowDocument).ToPlainText());
+            return l;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
